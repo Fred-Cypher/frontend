1c601fa2dd747497709070231b6e06b5263d233a
Fred-Cypher <frederique.perrier@gmail.com>
2021-08-31 17:27:05 +0200

    Ajout Bootstrap, début codage js

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+// js
+fetch('http://localhost:3000/api/teddies')
+    .then(response => response.json())
+    .then(data => {
+        const articlesContainer = document.getElementById('articles');
+        for (let article of data) {
+            articlesContainer.innerHTML += `${article.name} ${article.price}<br>`;
+        }
+    });
 
 
+    /*------------------ Header ------------------ */
+
+let logo = document.createElement("div");
+
+
+
+let headerTitle = document.createElement("div");
+
+let title = document.createElement("h1");
+
+title.textContent = "Orinoco";
+
+let enterpriseDescription = document.createElement("p");
+
+let basket = document.createElement("div");
+
+document.getElementById("header").appendChild(logo);
+document.getElementById("header").appendChild(headerTitle).appendChild(title);
+document.getElementById("header").appendChild(headerTitle).appendChild(enterpriseDescription);
+document.getElementById("header").appendChild(basket);
